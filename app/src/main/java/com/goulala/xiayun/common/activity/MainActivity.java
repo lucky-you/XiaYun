6519ab207e15a5e816ee2250b6b0506d22c19b82
Administrator <1021237228@qq.com>
2019-01-17 11:19:22 +0800

    签名处理

--- app/src/main/java/com/goulala/xiayun/common/activity/MainActivity.java
+++ app/src/main/java/com/goulala/xiayun/common/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.goulala.xiayun;
+package com.goulala.xiayun.common.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.goulala.xiayun.R;
 
 public class MainActivity extends AppCompatActivity {
 
